82c876146a85a3eb9af0a5b95b5df7740d9b07c8
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-10-29 14:12:57 +0100

    Update, order by date time and desc. Remove deadline for list

--- project/insert/insert_list.php
+++ project/insert/insert_list.php
@@ -1,7 +1,6 @@
 <?php
 require("../../bdd.php");
 if(isset($_POST['name_list']) and !empty($_POST['name_list'])){
-  if(isset($_POST['deadline_list']) and !empty($_POST['deadline_list'])){
     $req_insert = $bdd->prepare('INSERT INTO list (name, deadline, id_project, done) VALUES (:name, :deadline, :id_project, :done)');
     $req_insert->execute(array(
       'name' => $_POST['name_list'],
@@ -10,9 +9,6 @@ if(isset($_POST['name_list']) and !empty($_POST['name_list'])){
       'done' => 0
     ));
     header('Location: '. $_SERVER['HTTP_REFERER']);
-  } else {
-    echo "The deadline input cant not be empty.";
-  }
 } else {
   echo "The name of list cant not be empty.";
 }
